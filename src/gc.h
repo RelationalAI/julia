1ea684cc0e5f49b41b828c48143872c38d6d1e58
d-netto <diogonetto.dcn@gmail.com>
2023-11-03 14:06:34 -0300

    test 4k page size

--- src/gc.h
+++ src/gc.h
@@ -31,7 +31,7 @@
 extern "C" {
 #endif
 
-#define GC_PAGE_LG2 14 // log2(size of a page)
+#define GC_PAGE_LG2 12 // log2(size of a page)
 #define GC_PAGE_SZ (1 << GC_PAGE_LG2) // 16k
 #define GC_PAGE_OFFSET (JL_HEAP_ALIGNMENT - (sizeof(jl_taggedvalue_t) % JL_HEAP_ALIGNMENT))
 
@@ -235,16 +235,16 @@ STATIC_INLINE jl_gc_pagemeta_t *pop_lf_back(jl_gc_page_stack_t *pool) JL_NOTSAFE
 
 #ifdef _P64
 #define REGION0_PG_COUNT (1 << 16)
-#define REGION1_PG_COUNT (1 << 16)
+#define REGION1_PG_COUNT (1 << 18)
 #define REGION2_PG_COUNT (1 << 18)
-#define REGION0_INDEX(p) (((uintptr_t)(p) >> 14) & 0xFFFF) // shift by GC_PAGE_LG2
-#define REGION1_INDEX(p) (((uintptr_t)(p) >> 30) & 0xFFFF)
+#define REGION0_INDEX(p) (((uintptr_t)(p) >> 12) & 0xFFFF) // shift by GC_PAGE_LG2
+#define REGION1_INDEX(p) (((uintptr_t)(p) >> 28) & 0x3FFFF)
 #define REGION_INDEX(p)  (((uintptr_t)(p) >> 46) & 0x3FFFF)
 #else
-#define REGION0_PG_COUNT (1 << 8)
+#define REGION0_PG_COUNT (1 << 10)
 #define REGION1_PG_COUNT (1 << 10)
 #define REGION2_PG_COUNT (1 << 0)
-#define REGION0_INDEX(p) (((uintptr_t)(p) >> 14) & 0xFF) // shift by GC_PAGE_LG2
+#define REGION0_INDEX(p) (((uintptr_t)(p) >> 12) & 0x3FF) // shift by GC_PAGE_LG2
 #define REGION1_INDEX(p) (((uintptr_t)(p) >> 22) & 0x3FF)
 #define REGION_INDEX(p)  (0)
 #endif
