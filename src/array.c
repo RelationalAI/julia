992abe243d6beac02ff5ce430ec26d39be259b66
Diogo Netto <61364108+d-netto@users.noreply.github.com>
2024-05-03 11:51:49 -0300

    correctly track freed bytes in array_to_string (#54309) (#151)
    Should fix https://github.com/JuliaLang/julia/issues/54275.

--- src/array.c
+++ src/array.c
@@ -479,6 +479,7 @@ JL_DLLEXPORT jl_value_t *jl_array_to_string(jl_array_t *a)
             return o;
         }
     }
+    jl_gc_count_freed(jl_array_nbytes(a));
     a->nrows = 0;
     a->length = 0;
     return jl_pchar_to_string((const char*)jl_array_data(a), len);
