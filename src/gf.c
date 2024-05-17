0b7f7edeb6c1de131ef0df7a1963ce43c926afc4
Adnan Alhomssi <adnan.alhomssi@relational.ai>
2024-05-17 16:31:08 +0200

    tweak jl_coverage_touch_line

--- src/gf.c
+++ src/gf.c
@@ -3980,12 +3980,15 @@ JL_DLLEXPORT void jl_typeinf_lock_end(void)
     JL_UNLOCK(&jl_codegen_lock);
 }
 
-// TODO: Where to put this function?
+
 JL_DLLEXPORT void jl_coverage_touch_line(uint64_t *ptr, int64_t addend) {
     uint64_t v = *ptr;
-    // Huh, apparently the first time this is called, `*ptr` is `1`, not `0`?
+    // cov = 1 --> instrumented not hit. --> 2 covered
     if (v <= 10) {
         *ptr = v + addend;
+        return;
+    } else {
+        return;
     }
 }
 
